fe0e992e7d0cdba4f10e72d6f8a764614aed9e2d
DJaySky <feylinurala@gmail.com>
2025-05-02 23:15:51 -0400

    Initial commit

---
+++ install-conf.sh
@@ -0,0 +1,16 @@
+# i3
+cp i3/i3conf ~/.config/i3/config
+cp i3/statusconf ~/.config/i3status/config
+
+# neovim
+# install vim-plug:
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp misc/initvim ~/.config/nvim/init.vim
+
+# other configs
+cp misc/htoprc ~/.config/htop/htoprc
+cp misc/Xresources ~/.Xresources
+cp misc/nanorc ~/.nanorc
+cp misc/bashrc ~/.bashrc
+cp misc/tmuxconf ~/.tmux.conf
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script is specifically tailored for Void Linux. If you just want the configs,
+# run install-conf.sh on your own.
+
+set -e
+
+# install all of my programs
+sudo xbps-install nano neovim xclip pulseaudio bluez i3 i3-gaps feh dmenu tmux flatpak curl rxvt-unicode-9.31_5 xrdb dolphin git github-cli xrandr htop neofetch gcc make spectacle xset
+
+# enable relevant services
+sudo ln -s /etc/sv/bluetoothd /var/service/
+sudo ln -s /etc/sv/dbus /var/service/
+
+# makes most used directories
+mkdir ~/Projects
+mkdir ~/Sources
+
+# installs configs
+chmod +x ./install-conf.sh
+./install-conf.sh
+
+# disable automatic power management on the wifi card
+if [ ! -d "/etc/NetworkManager" ]; then
+	sudo mkdir /etc/NetworkManager
+fi
+
+if [ ! -d "/etc/NetworkManager/conf.d" ]; then
+	sudo mkdir /etc/NetworkManager/conf.d
+fi
+
+echo "[connection]\nwifi.powersave = 2" | sudo tee /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf > /dev/null
